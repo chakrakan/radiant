156ce271c9b50a04e80451642da7cdd6219f4042
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2023-07-14 03:47:26 -0400

    update

--- go.mod
+++ go.mod
@@ -1,16 +1,28 @@
 module github.com/chakrakan/radiant
 
-go 1.15
+go 1.20
+
+require github.com/gocolly/colly v1.2.0
+
+require github.com/google/go-cmp v0.5.6 // indirect
 
 require (
-	github.com/PuerkitoBio/goquery v1.6.1 // indirect
-	github.com/antchfx/htmlquery v1.2.3 // indirect
-	github.com/antchfx/xmlquery v1.3.5 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.0 // indirect
+	github.com/antchfx/xmlquery v1.3.17 // indirect
+	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
-	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- radiant.go
+++ radiant.go
@@ -1,68 +1,149 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
 )
 
-// Profile stores information about a valorant profile from blitz.gg
+const (
+	baseURL string = "https://tracker.gg/valorant/profile/riot/%s/overview"
+	gistName string = "Valorant Stats"
+)
+
+// Profile stores information about a valorant profile from tracker.gg
 type Profile struct {
 	Name        string
-	LastPlayed  string
-	KdRatio     string
-	HsPercent   string
-	DmgPerRound string
-	CombatScore string
-	RankSvgURL  string
+	KdRatio     float64
+	HsPercent   float64
+	Wins 		int
+	Losses 		int
+	AvgCombatScore float64
+	CurrentRank string
+	PeakRank string
+	CurrentRankPngURL string
+	PeakRankPngURL  string
+}
+
+func (p *Profile) setPeakAndCurrentRankInfo(e *colly.HTMLElement) {
+	if strings.Contains(e.Text, "Peak") {
+		splitRankAndRating := strings.Split(e.Text, "    ")[0]
+		p.PeakRank = strings.Split(splitRankAndRating, "Rating")[1]
+		p.PeakRankPngURL = e.ChildAttr("img", "src")
+	} else {
+		p.CurrentRank = strings.Split(e.Text, "Rating")[1]
+		p.CurrentRankPngURL = e.ChildAttr("img", "src")
+	}
+}
+
+func (p *Profile) setWinsAndLosses(e *colly.HTMLElement) {
+	cleanUp := strings.TrimRight(e.Text, "WL")
+	trimSpace := strings.TrimSpace(cleanUp)
+	splitText := strings.Split(trimSpace, "  ")
+	wins, err := strconv.Atoi(splitText[0])
+	if err != nil {
+		log.Printf("Error converting wins: %s", err.Error())
+	}
+	p.Wins = wins
+
+	losses, err := strconv.Atoi(splitText[1])
+	if err != nil {
+		log.Printf("Error converting losses: %s", err.Error())
+	}
+	p.Losses = losses
+}
+
+func (p *Profile) setNumericalInfo(e *colly.HTMLElement) {
+	switch {
+		case strings.Contains(e.Text, "ACS"):
+			cleanupLeft := strings.TrimLeft(e.Text, "ACS")
+			acsValue, err := strconv.ParseFloat(strings.Split(cleanupLeft, "Top")[0], 64)
+			if err != nil {
+				log.Println("Unable to parse ACS")
+			}
+			p.AvgCombatScore = acsValue
+		case strings.Contains(e.Text, "K/D Ratio"):
+			cleanupLeft := strings.TrimLeft(e.Text, "K/D Ratio")
+			kdRatio, err := strconv.ParseFloat(strings.Split(cleanupLeft, "Top")[0], 64)
+			if err != nil {
+				log.Println("Unable to parse K/D Ratio")
+			}
+			p.KdRatio = kdRatio
+		case strings.Contains(e.Text, "Headshot"):
+			cleanupLeft := strings.TrimLeft(e.Text, "Headshot%")
+			hsPercentage, err := strconv.ParseFloat(strings.Split(cleanupLeft, "%")[0], 64)
+			if err != nil {
+				log.Println("Unable to parse Headshot Percentage")
+			}
+			p.HsPercent = hsPercentage
+	}
 }
 
 func main() {
-	profileID := "adorn-1625"
-
-	if profileID == "" {
-		log.Println("Profile post id required")
-		os.Exit(1)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	// Instantiate default collector
+	var (
+		githubToken = os.Getenv("GITHUB_TOKEN")
+		profileID = os.Getenv("TRACKER_PROFILE_ID")
+	)
+
+	if profileID == "" || githubToken == "" {
+		log.Fatal("Please ensure you have the correct env variables set")
+	}
+
+	targetURL := fmt.Sprintf(baseURL, url.QueryEscape(profileID))
+
+	// setup Profile struct
+	profile := Profile{Name: profileID}
+
+	// Instantiate default collector and scrape
 	c := colly.NewCollector(
 		// Visit only domains: blitz.gg, www.blitz.gg
-		colly.AllowedDomains("blitz.gg", "www.blitz.gg"),
-
+		colly.AllowedDomains("tracker.gg", "www.tracker.gg"),
 		// Cache responses to prevent multiple download of pages
 		// even if the collector is restarted
 		colly.CacheDir("./radiant_cache"),
 	)
 
-	detailCollector := c.Clone()
-
-	profiles := make([]Profile, 0, 200)
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		log.Println(e.Attr("href"))
+	// Get ranks
+	c.OnHTML("div.rating-summary__content", func(e *colly.HTMLElement) {
+		profile.setPeakAndCurrentRankInfo(e)
 	})
 
-	// Before making a request print "Visiting ..."
+	// Get Numerical data other than wins/losses
+	c.OnHTML("div.numbers", func(e *colly.HTMLElement) {
+		profile.setNumericalInfo(e)
+	})
+
+	// Get Wins/Losses
+	c.OnHTML("div.trn-profile-highlighted-content__ratio", func(e *colly.HTMLElement) {
+		profile.setWinsAndLosses(e)
+	})
+
 	c.OnRequest(func(r *colly.Request) {
-		log.Println("visiting", r.URL.String())
+		log.Printf("Visiting %s\n", r.URL)
 	})
 
-	// Extract details of the profile
-	detailCollector.OnHTML(`div[id=blitz-app]`, func(e *colly.HTMLElement) {
-		log.Println("Profile found", e.Request.URL)
-		name := e.ChildText(".profile-info")
-		if name == "" {
-			log.Println("No profile found", e.Request.URL)
-		}
-		profile := Profile{
-			Name:       name,
-			LastPlayed:         e.Request.URL.String(),
-		}
-		profiles = append(profiles, profile)
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Error while scraping: %s\n", err.Error())
 	})
 
-	c.Visit("https://blitz.gg/valorant/profile/" + profileID)
+	c.Visit(targetURL)
+
+	fmt.Println(profile)
+
+	// setup Gist
+	// content := &github.Gist{Files: map[github.GistFilename]github.GistFile{
+	// 	github.GistFilename(gistName): {Content: github.String(title)},
+	// }}
 
 }
